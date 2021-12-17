00b1a02af5857ba7fcc1081ac6af6807277b6e4d
ADM21JF008009 <92528233+ADM21JF-008009@users.noreply.github.com>
2021-12-17 17:07:55 +0530

    Delete Stage 3 Spring data JPA Handsons directory

--- Stage 3 Spring data JPA Handsons/Day 1 - Session 1 and 2/orm-learn/src/main/java/com/cognizant/springlearn/service/exception/CountryNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.cognizant.springlearn.service.exception;
-public class CountryNotFoundException extends Exception {
-	public void CountryNotFoundException(String msg) {
-		super(msg);
-	}
-}
