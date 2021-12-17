c5221c93037ed6b3e08c7f1b33a9e230fd0c65a5
ADM21JF008009 <92528233+ADM21JF-008009@users.noreply.github.com>
2021-12-17 16:34:02 +0530

    Add files via upload

---
+++ Stage 3 Spring data JPA Handsons/Day 1 - Session 1 and 2/orm-learn/src/main/java/com/cognizant/springlearn/service/exception/CountryNotFoundException.java
@@ -0,0 +1,6 @@
+package com.cognizant.springlearn.service.exception;
+public class CountryNotFoundException extends Exception {
+	public void CountryNotFoundException(String msg) {
+		super(msg);
+	}
+}
